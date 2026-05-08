fd2058c3ed41f4c2a654eebf580236e2164b4b50
Gugan <131288000+Gugan-23@users.noreply.github.com>
2026-05-08 21:51:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,15 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/volt-e
 app.use(cors({
   origin: "*"
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({
+  limit: '50mb'
+}));
+
+app.use(express.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Connect to MongoDB
